a6eb2e368da9290dc78c77461cda5abb508fc200
DeveloperBoyAman <108537150+DeveloperBoyAman@users.noreply.github.com>
2023-01-13 21:24:36 +0530

    Add files via upload

---
+++ Programs In C/Input Output/printCWithAman.c
@@ -0,0 +1,16 @@
+// C Program to Print C With Aman
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+
+// Main Function Start
+int main()
+{
+
+  printf("C With Aman"); // Formatted Output Function
+
+  getch();
+  return 0;
+}
+// Main Function End
