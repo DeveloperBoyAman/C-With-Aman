2a7cc25a6111b68589df5a529a34762d51696137
DeveloperBoyAman <108537150+DeveloperBoyAman@users.noreply.github.com>
2023-01-13 22:08:21 +0530

    Create first10OddNaturalNumbersInReverse.c

---
+++ Programs In C/Array/first10OddNaturalNumbersInReverse.c
@@ -0,0 +1,34 @@
+// C Program Print First 10 Odd Natural Numbers In Reverse Order
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+
+// Main Function Start
+
+int main()
+{
+
+    // Using While Loop
+    // int i = 10;
+    // printf("\n>>>>>>>>>>>>> First 10 Odd Natural Numbers In Reverse Order <<<<<<<<<<<<<<\n");
+    // while (i > 0)
+    //     printf("%d ", i-- * 2 - 1);
+
+    // Using For Loop
+    printf("\n>>>>>>>>>>>>> First 10 Odd Natural Numbers In Reverse Order <<<<<<<<<<<<<<\n");
+    for (int i = 10; i > 0; i--)
+        printf("%d ", i * 2 - 1);
+
+    // Using Do While Loop
+    // int i = 10;
+    // printf("\n>>>>>>>>>>>>> First 10 Odd Natural Numbers In Reverse Order <<<<<<<<<<<<<<\n");
+    // do
+    // {
+    //     printf("%d ", i-- * 2 - 1);
+    // } while (i > 0);
+
+    getch();
+    return 0;
+}
+// Main Function End
