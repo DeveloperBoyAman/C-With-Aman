88c062c598365b5cd81472facf53d7862f17f658
DeveloperBoyAman <108537150+DeveloperBoyAman@users.noreply.github.com>
2023-01-14 09:35:21 +0530

    Create useOfNewLineCharacter.c

---
+++ Programs In C/Input Output/useOfNewLineCharacter.c
@@ -0,0 +1,16 @@
+// C Program to Print C With in "First Line" and "Aman" in Second Line ( Use of '\n')
+
+// Header Files
+#include<stdio.h>
+#include<conio.h>
+
+// Main Function Start
+int main(){
+
+    printf("C With\nAman");
+   
+    getch();
+    return 0;
+
+}
+// Main Function End
