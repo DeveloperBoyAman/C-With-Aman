e97955621541c395e7255b53534a548f26e75f53
DeveloperBoyAman <108537150+DeveloperBoyAman@users.noreply.github.com>
2023-01-14 10:23:48 +0530

    Create natureOfRoots.c

---
+++ Programs In C/Decision Control Statements/natureOfRoots.c
@@ -0,0 +1,26 @@
+// C Program to Find Roots of a Quadratic Equation are Real and Distinct , Real and Equal or Imaginary
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+
+// Main Function Start
+
+int main()
+{
+
+    float a, b, c, discriminant;
+    printf("\nEnter Coefficients a,b and c => ");
+    scanf("%f%f%f", &a, &b, &c);
+    discriminant = b * b - 4 * a * c;
+    if (discriminant > 0)
+        printf("Roots Are Real and Distnict");
+    else if (discriminant)
+        printf("Roots Are Real and Equal");
+    else
+        printf("Roots Are Imaginary");
+
+    getch();
+    return 0;
+}
+// Main Function End
