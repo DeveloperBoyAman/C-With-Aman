88f7c84909b952e0ffc92659c4feb748715f6bfc
DeveloperBoyAman <108537150+DeveloperBoyAman@users.noreply.github.com>
2023-01-14 10:39:00 +0530

    Create printCWithAman5TimesOnScreen.c

---
+++ Programs In C/Iterative Control Statements (Loops)/printCWithAman5TimesOnScreen.c
@@ -0,0 +1,35 @@
+// C Program Print Web With Aman 5 Times On the Screen
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+
+// Main Function Start
+
+int main()
+{
+
+    // Using While Loop
+    // int i = 1;
+    // while (i <= 5)
+    // {
+    //     printf("\nWeb With Aman");
+    //     i++;
+    // }
+
+    // Using For Loop
+    for (int i = 1; i <= 5; i++)
+        printf("\nWeb With Aman");
+
+    // Using Do While Loop
+    // int i = 1;
+    // do
+    // {
+    //     printf("\nWeb With Aman");
+    //     i++;
+    // } while (i <= 5);
+
+    getch();
+    return 0;
+}
+// Main Function End
