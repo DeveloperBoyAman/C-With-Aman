444569b89fdf9bcbd6f57b177364834e1cd7acba
DeveloperBoyAman <108537150+DeveloperBoyAman@users.noreply.github.com>
2023-01-14 09:53:55 +0530

    Create findSizeOfVariables.c

---
+++ Programs In C/Operators/findSizeOfVariables.c
@@ -0,0 +1,21 @@
+// C Program to Print Size of an int , Float , Char , Double and Long Double Type Variable
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+
+// Main Function Start
+
+int main()
+{
+
+    printf("\nSize of int Variable => %d Byte\n", sizeof(int));
+    printf("Size of char Variable => %d Byte\n", sizeof(char));
+    printf("Size of float Variable => %d Byte\n", sizeof(float));
+    printf("Size of double Variable => %d Byte\n", sizeof(double));
+    printf("Size of double Variable => %d Byte\n", sizeof(long double));
+
+    getch();
+    return 0;
+}
+// Main Function End
