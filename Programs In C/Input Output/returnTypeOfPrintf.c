42750c84fb7706e2645f3b78e8ff032582d0be07
DeveloperBoyAman <108537150+DeveloperBoyAman@users.noreply.github.com>
2023-01-14 09:38:55 +0530

    Create returnTypeOfPrintf.c

---
+++ Programs In C/Input Output/returnTypeOfPrintf.c
@@ -0,0 +1,20 @@
+// C Program to Find Length of String Using Print() Function
+
+// Header Files
+
+#include <stdio.h>
+#include <conio.h>
+
+// Main Function Start
+
+int main()
+{
+
+    int length;
+    length = printf("Welcome"); // Return the Total Number ofd Characters Successfully Printed On Screen or Console
+    printf("\nTotal Characters in \"Welcome\" Word => %d", length);
+
+    getch();
+    return 0;
+}
+// Main Function End
