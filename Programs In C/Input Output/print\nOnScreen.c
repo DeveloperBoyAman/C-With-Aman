b6653ea47970012d2a484e771b917f6a7cabad8e
DeveloperBoyAman <108537150+DeveloperBoyAman@users.noreply.github.com>
2023-01-14 09:41:29 +0530

    Create print\nOnScreen.c

---
+++ Programs In C/Input Output/print\nOnScreen.c
@@ -0,0 +1,18 @@
+// C Program to Print \n On Screen
+
+// Header Files
+
+#include <stdio.h>
+#include <conio.h>
+
+// Main Function Start
+
+int main()
+{
+
+    printf("\\n");
+
+    getch();
+    return 0;
+}
+// Main Function End
