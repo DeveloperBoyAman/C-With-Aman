f8e8b71aedc2ad801a35d76f1be65ef2dbc84682
DeveloperBoyAman <108537150+DeveloperBoyAman@users.noreply.github.com>
2023-01-14 09:51:34 +0530

    Create printASCIICode.c

---
+++ Programs In C/Operators/printASCIICode.c
@@ -0,0 +1,20 @@
+// C Program to Take A character and Print its ASCII Code 
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+
+// Main Function Start
+
+int main()
+{
+
+    char ch;
+    printf("\nEnter A Character => ");
+    scanf("%c", &ch);
+    printf("\nASCII Code of %c is %d", ch, ch);
+
+    getch();
+    return 0;
+}
+// Main Function End
