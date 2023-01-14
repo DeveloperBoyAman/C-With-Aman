508d24ffb963b53c42ff02a49c9cc95de7c95163
DeveloperBoyAman <108537150+DeveloperBoyAman@users.noreply.github.com>
2023-01-14 09:40:49 +0530

    Create print\\OnScreen.c

---
+++ Programs In C/Input Output/print\\OnScreen.c
@@ -0,0 +1,18 @@
+// C Program to Print \\ On Screen
+
+// Header Files
+
+#include <stdio.h>
+#include <conio.h>
+
+// Main Function Start
+
+int main()
+{
+
+    printf("\\\\");
+
+    getch();
+    return 0;
+}
+// Main Function End
