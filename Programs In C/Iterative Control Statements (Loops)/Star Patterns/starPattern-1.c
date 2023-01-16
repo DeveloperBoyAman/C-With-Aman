0dcf45032dcca6a7c206f8a9d37eae096249cc34
DeveloperBoyAman <108537150+DeveloperBoyAman@users.noreply.github.com>
2023-01-16 09:59:21 +0530

    Create starPattern-1.c

---
+++ Programs In C/Iterative Control Statements (Loops)/Star Patterns/starPattern-1.c
@@ -0,0 +1,53 @@
+// C Program to Print Star Pattern
+
+/*
+
+*
+**
+***
+****
+*****
+
+*/
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+
+// Main Function Start
+int main()
+{
+
+    /**************** 1st Approach *******************/
+    int rows;
+    printf("\nHow Many Rows => ");
+    scanf("%d", &rows);
+    putch('\n');
+    for (int i = 1; i <= rows; i++)
+    {
+        for (int j = 1; j <= i; j++)
+            printf("*");
+        putch('\n');
+    }
+
+    /**************** 2nd Approach *******************/
+    // int rows;
+    // printf("\nHow Many Rows => ");
+    // scanf("%d", &rows);
+    // putch('\n');
+    // for (int i = 1; i <= rows; i++)
+    // {
+    //     for (int j = 1; j <= rows; j++)
+    //     {
+    //         if (j <= i)
+    //             printf("*");
+    //         else
+    //             printf(" ");
+    //     }
+    //     putch('\n');
+    // }
+
+    getch();
+    return 0;
+}
+// Main Function End
