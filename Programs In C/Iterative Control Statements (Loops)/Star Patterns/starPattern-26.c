a6823586ac19f84ee9192982a5c937eaee6b9118
Aman Kumar <108537150+DeveloperBoyAman@users.noreply.github.com>
2023-02-19 14:22:20 +0530

    Create starPattern-26.c

---
+++ Programs In C/Iterative Control Statements (Loops)/Star Patterns/starPattern-26.c
@@ -0,0 +1,58 @@
+// Star Pattern
+
+/*
+
+    Pattern 26.
+
+        1
+      2 2
+    3 3 3
+  4 4 4 4
+
+*/
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+// Main Function Start
+int main()
+{
+
+    // // 1st Approach
+    // int rows, cols;
+    // printf("\nHow Many Rows => ");
+    // scanf("%d", &rows);
+    // cols = rows;
+    // puts("\n--------------------------------------------\n");
+    // for (int row = 1; row <= rows; row++)
+    // {
+    //     for (int col = 1; col <= cols; col++)
+    //     {
+    //         if (col >= rows + 1 - row)
+    //             printf("%d", row);
+    //         else
+    //             printf(" ");
+    //     }
+    //     printf("\n");
+    // }
+
+    // // 2nd Approach
+    int rows;
+    printf("\nHow Many Rows => ");
+    scanf("%d", &rows);
+    puts("\n--------------------------------------------\n");
+    for (int row = 1; row <= rows; row++)
+    {
+        for (int space = 1; space <= rows - row; space++)
+            printf(" ");
+        for (int col = 1; col <= row; col++)
+            printf("%d", row);
+        printf("\n");
+    }
+
+    getch();
+    return 0;
+}
+// Main Function End
