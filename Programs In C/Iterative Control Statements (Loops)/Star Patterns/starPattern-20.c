21c088fd5bc5197f1cd676eddc63b4bc0f80b6eb
Aman Kumar <108537150+DeveloperBoyAman@users.noreply.github.com>
2023-02-19 13:03:07 +0530

    Create starPattern-20.c

---
+++ Programs In C/Iterative Control Statements (Loops)/Star Patterns/starPattern-20.c
@@ -0,0 +1,53 @@
+// Star Pattern
+
+/*
+
+    Pattern 20.
+
+    1
+    2 3
+    4 5 6
+    7 8 9 10
+
+*/
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+// Main Function Start
+int main()
+{
+
+    // // 1st Approach
+    int rows;
+    printf("\nHow Many Rows => ");
+    scanf("%d", &rows);
+    puts("\n--------------------------------------------\n");
+    int count = 1;
+    for (int row = 1; row <= rows; row++)
+    {
+        for (int col = 1; col <= row; col++)
+            printf("%2d ", count++);
+        printf("\n");
+    }
+
+    // // 2nd Approach
+    // int rows;
+    // printf("\nHow Many Rows => ");
+    // scanf("%d", &rows);
+    // int count = 1;
+    // puts("\n--------------------------------------------\n");
+    // for (int row = 1; row <= rows; row++)
+    // {
+    //     count += row - 1;
+    //     for (int col = 1; col <= row; col++)
+    //         printf("%2d ", count + col - 1);
+    //     printf("\n");
+    // }
+
+    getch();
+    return 0;
+}
+// Main Function End
