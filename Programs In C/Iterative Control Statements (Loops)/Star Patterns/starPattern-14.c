6a2885d2c458d0342f0d05063c32728a9f3d1477
Aman Kumar <108537150+DeveloperBoyAman@users.noreply.github.com>
2023-02-20 20:54:50 +0530

    Update starPattern-14.c

--- Programs In C/Iterative Control Statements (Loops)/Star Patterns/starPattern-14.c
+++ Programs In C/Iterative Control Statements (Loops)/Star Patterns/starPattern-14.c
@@ -1,1 +1,63 @@
+// Star Pattern
 
+/*
+
+     Pattern 14.
+
+     *
+     * *
+     *   *
+     *     *
+     * * * * *
+
+  */
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+// Main Function Start
+int main()
+{
+
+    // // 1st Approach
+    // int rows, cols;
+    // printf("\nHow Many Rows => ");
+    // scanf("%d", &rows);
+    // cols = rows;
+    // puts("\n--------------------------------------------\n");
+    // for (int row = 1; row <= rows; row++)
+    // {
+    //     for (int col = 1; col <= cols; col++)
+    //     {
+    //         if (col == 1 || col == row || row == rows)
+    //             printf("*");
+    //         else
+    //             printf(" ");
+    //     }
+    //     printf("\n");
+    // }
+
+    // // 2nd Approach
+    int rows, cols;
+    printf("\nHow Many Rows => ");
+    scanf("%d", &rows);
+    cols = rows;
+    puts("\n--------------------------------------------\n");
+    for (int row = 1; row <= rows; row++)
+    {
+        for (int col = 1; col <= row; col++)
+        {
+            if (col == 1 || col == row || row == rows)
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+
+    getch();
+    return 0;
+}
+// Main Function End
