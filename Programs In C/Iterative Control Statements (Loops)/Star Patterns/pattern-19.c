a0a74cd28e4f2ae84629110d86e0155c78500371
Aman Kumar <108537150+DeveloperBoyAman@users.noreply.github.com>
2023-03-02 20:09:53 +0530

    Update and rename starPattern-19.c to pattern-19.c

---
+++ Programs In C/Iterative Control Statements (Loops)/Star Patterns/pattern-19.c
@@ -0,0 +1,68 @@
+// C Program to Print Following Pattern
+
+/*
+
+    Pattern 19.
+
+  *****     *****
+ *******   *******
+********* *********
+*******AMAN********
+ *****************
+  ***************
+   *************
+    ***********
+     *********
+      *******
+       *****
+        ***
+         *
+
+
+*/
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+
+// Main Function Start
+int main()
+{
+
+    int rows = 13, cols = 19;
+    for (int row = 1; row <= rows; row++)
+    {
+        for (int col = 1; col <= cols; col++)
+        {
+            if (row <= 3)
+            {
+                if (col >= 4 - row && col <= 7 - 1 + row || col >= 14 - row && col <= 17 - 1 + row)
+                    printf("*");
+                else
+                    printf(" ");
+            }
+            else if (row == 4)
+            {
+                if (col <= 7 || col >= 12)
+                    printf("*");
+                else
+                {
+                    printf("AMAN");
+                    col += 3;
+                }
+            }
+            else
+            {
+                if (col >= row - 3 && col <= cols - (row - 4))
+                    printf("*");
+                else
+                    printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    getch();
+    return 0;
+}
+// Main Function End
