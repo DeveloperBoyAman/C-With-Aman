d3234fc1a075923d2272ca647dc13f6bf4e5cf24
Aman Kumar <108537150+DeveloperBoyAman@users.noreply.github.com>
2023-03-03 18:18:48 +0530

    Update starPattern-23.c

--- Programs In C/Iterative Control Statements (Loops)/Star Patterns/starPattern-23.c
+++ Programs In C/Iterative Control Statements (Loops)/Star Patterns/starPattern-23.c
@@ -1,25 +1,25 @@
-// Star Pattern
+// C Program to Print Following Pattern
 
 /*
 
     Pattern 23.
 
-    A B C
-    D E F
-    G H I
-
+    A B C D
+    E F G H
+    I J K L
+    M N O P
 
 */
 
 // Header Files
 #include <stdio.h>
 #include <conio.h>
-#include <stdlib.h>
 
 // Main Function Start
 int main()
 {
 
+    // // 1st Approach
     int rows;
     char ch = 'A';
     printf("\nHow Many Rows => ");
@@ -31,6 +31,21 @@ int main()
             printf("%c ", ch++);
         printf("\n");
     }
+
+    // // 2nd Approach
+    // int rows;
+    // char ch = 'A';
+    // printf("\nHow Many Rows => ");
+    // scanf("%d", &rows);
+    // ch -= rows;
+    // puts("\n--------------------------------------------\n");
+    // for (int row = 1; row <= rows; row++)
+    // {
+    //     ch += rows;
+    //     for (int col = 1; col <= rows; col++)
+    //         printf("%c ", ch - 1 + col);
+    //     printf("\n");
+    // }
 
     getch();
     return 0;
