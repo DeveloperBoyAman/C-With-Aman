cab60afd2add34db78758722e6ca0b9dd96884f7
Aman Kumar <108537150+DeveloperBoyAman@users.noreply.github.com>
2023-03-03 18:05:11 +0530

    Update starPattern-21.c

--- Programs In C/Iterative Control Statements (Loops)/Star Patterns/starPattern-21.c
+++ Programs In C/Iterative Control Statements (Loops)/Star Patterns/starPattern-21.c
@@ -1,4 +1,4 @@
-// Star Pattern
+// C Program to Print Following Pattern
 
 /*
 
@@ -15,7 +15,6 @@
 // Header Files
 #include <stdio.h>
 #include <conio.h>
-#include <stdlib.h>
 
 // Main Function Start
 int main()
@@ -29,11 +28,24 @@ int main()
     for (int row = 1; row <= rows; row++)
     {
         for (int col = 1; col <= row; col++)
-            printf("%2d ", row + col - 1);
+            printf("%-2d ", row + col - 1);
         printf("\n");
     }
 
     // // 2nd Approach
+    // int rows, num;
+    // printf("\nHow Many Rows => ");
+    // scanf("%d", &rows);
+    // puts("\n--------------------------------------------\n");
+    // for (int row = 1; row <= rows; row++)
+    // {
+    //     num = row;
+    //     for (int col = 1; col <= row; col++)
+    //         printf("%-2d ", num++);
+    //     printf("\n");
+    // }
+
+    // // 3rd Approach
     // int rows;
     // printf("\nHow Many Rows => ");
     // scanf("%d", &rows);
@@ -41,7 +53,7 @@ int main()
     // for (int row = 1; row <= rows; row++)
     // {
     //     for (int col = row; col <= row * 2 - 1; col++)
-    //         printf("%2d ", col);
+    //         printf("%-2d ", col);
     //     printf("\n");
     // }
 
