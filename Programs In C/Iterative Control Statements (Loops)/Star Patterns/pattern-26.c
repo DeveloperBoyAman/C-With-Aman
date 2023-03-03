7530f56672df7d317c2617e762c0c07027f8bfbf
Aman Kumar <108537150+DeveloperBoyAman@users.noreply.github.com>
2023-03-03 18:40:25 +0530

    Update pattern-26.c

--- Programs In C/Iterative Control Statements (Loops)/Star Patterns/pattern-26.c
+++ Programs In C/Iterative Control Statements (Loops)/Star Patterns/pattern-26.c
@@ -1,1 +1,78 @@
+// C Program to Print Following Pattern
 
+/*
+
+    Pattern 26.
+
+       1 2 3 4
+         2 3 4
+           3 4
+             4
+
+*/
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+
+// Main Function Start
+int main()
+{
+
+  // // 1st Approach
+  int rows, cols, num;
+  printf("\nHow Many Rows => ");
+  scanf("%d", &rows);
+  cols = rows;
+  puts("\n--------------------------------------------\n");
+  for (int row = 1; row <= rows; row++)
+  {
+    num = row;
+    for (int col = 1; col <= cols; col++)
+    {
+      if (col >= row)
+        printf("%d ", num++);
+      else
+        printf("  ");
+    }
+    printf("\n");
+  }
+
+  // // 2nd Approach
+  // int rows, cols;
+  // printf("\nHow Many Rows => ");
+  // scanf("%d", &rows);
+  // cols = rows;
+  // puts("\n--------------------------------------------\n");
+  // for (int row = 1; row <= rows; row++)
+  // {
+  //   for (int col = 1; col <= cols; col++)
+  //   {
+  //     if (col >= row)
+  //       printf("%d ", col);
+  //     else
+  //       printf("  ");
+  //   }
+  //   printf("\n");
+  // }
+
+  // // 3rd Approach
+  // int rows;
+  // printf("\nHow Many Rows => ");
+  // scanf("%d", &rows);
+  // puts("\n--------------------------------------------\n");
+  // for (int row = 1; row <= rows; row++)
+  // {
+  //   for (int space = 1; space <= row - 1; space++)
+  //     printf("  ");
+
+  //   for (int col = row; col <= rows; col++)
+  //     printf("%d ", col);
+
+  //   printf("\n");
+  // }
+
+  getch();
+  return 0;
+}
+// Main Function End
