6b1f84377fb9990b31246a9609ee6b89b34c79e9
Aman Kumar <108537150+DeveloperBoyAman@users.noreply.github.com>
2023-03-04 23:42:03 +0530

    Update pattern-40.c

--- Programs In C/Iterative Control Statements (Loops)/Star Patterns/pattern-40.c
+++ Programs In C/Iterative Control Statements (Loops)/Star Patterns/pattern-40.c
@@ -1,1 +1,85 @@
+// Star Pattern
 
+/*
+
+    Pattern 40.
+
+       *        *
+       **      **
+       ***    ***
+       ****  ****
+       **********
+       ****  ****
+       ***    ***
+       **      **
+       *        *
+
+
+*/
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+
+// Main Function Start
+int main()
+{
+
+    // //  1st Approach
+    int rows, cols, midOfRows, k = 1;
+    printf("\nHow Many Rows => ");
+    scanf("%d", &rows);
+    midOfRows = (rows + 1) / 2;
+    cols = midOfRows * 2;
+
+    puts("\n--------------------------------------------\n");
+    for (int row = 1; row <= rows; row++)
+    {
+        for (int col = 1; col <= cols; col++)
+        {
+            if (col <= k || col >= cols + 1 - k)
+                printf("*");
+            else
+                printf(" ");
+        }
+
+        // If User Wants Even Number of Rows
+        if (row == midOfRows && rows % 2 == 0)
+            k++;
+
+        row < midOfRows ? k++ : k--;
+
+        printf("\n");
+    }
+
+    // // 2nd Approach
+    // int rows, starsInEachRow, midOfRows, k;
+    // printf("\nHow Many Rows => ");
+    // scanf("%d", &rows);
+    // midOfRows = (rows + 1) / 2;
+    // puts("\n--------------------------------------------\n");
+    // for (int row = 1; row <= rows; row++)
+    // {
+
+    //     starsInEachRow = (row <= midOfRows ? row : midOfRows * 2 - row) * 2;
+
+    //     // If User Wants Even Number of Rows
+    //     if (rows % 2 == 0 && row > midOfRows)
+    //         starsInEachRow += 2;
+
+    //     for (int col = 1; col <= starsInEachRow / 2; col++)
+    //         printf("*");
+
+    //     for (int space = 1; space <= midOfRows * 2 - starsInEachRow; space++)
+    //         printf(" ");
+
+    //     for (int col = 1; col <= starsInEachRow / 2; col++)
+    //         printf("*");
+
+    //     printf("\n");
+    // }
+
+    getch();
+    return 0;
+}
+// Main Function End
