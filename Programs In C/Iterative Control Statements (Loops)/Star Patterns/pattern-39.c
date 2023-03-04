bf7ec8e26d88c78b09f04adc5b473650753d96a5
Aman Kumar <108537150+DeveloperBoyAman@users.noreply.github.com>
2023-03-04 23:27:05 +0530

    Update and rename starPattern-39.c to pattern-39.c

---
+++ Programs In C/Iterative Control Statements (Loops)/Star Patterns/pattern-39.c
@@ -0,0 +1,79 @@
+// Star Pattern
+
+/*
+
+    Pattern 39.
+
+      **********
+      ****  ****
+      ***    ***
+      **      **
+      *        *
+      *        *
+      **      **
+      ***    ***
+      ****  ****
+      **********
+
+
+*/
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+
+// Main Function Start
+int main()
+{
+
+    // //  1st Approach
+    int rows, cols, midOfRows, k = 1;
+    printf("\nHow Many Rows => ");
+    scanf("%d", &rows);
+    cols = rows * 2;
+    rows *= 2;
+    midOfRows = rows / 2;
+    puts("\n--------------------------------------------\n");
+    for (int row = 1; row <= rows; row++)
+    {
+        for (int col = 1; col <= cols; col++)
+        {
+            if (col <= midOfRows + 1 - k || col >= midOfRows + k)
+                printf("*");
+            else
+                printf(" ");
+        }
+        if (row == midOfRows)
+            k++;
+        row < midOfRows ? k++ : k--;
+        printf("\n");
+    }
+
+    // // 2nd Approach
+    // int rows, starsInEachRow, midOfRows;
+    // printf("\nHow Many Rows => ");
+    // scanf("%d", &rows);
+    // rows *= 2;
+    // midOfRows = rows / 2;
+    // puts("\n--------------------------------------------\n");
+    // for (int row = 1; row <= rows; row++)
+    // {
+    //     starsInEachRow = (row <= midOfRows ? midOfRows + 1 - row : row - midOfRows) * 2;
+
+    //     for (int col = 1; col <= starsInEachRow / 2; col++)
+    //         printf("*");
+
+    //     for (int space = 1; space <= rows - starsInEachRow; space++)
+    //         printf(" ");
+
+    //     for (int col = 1; col <= starsInEachRow / 2; col++)
+    //         printf("*");
+
+    //     printf("\n");
+    // }
+
+    getch();
+    return 0;
+}
+// Main Function End
+
