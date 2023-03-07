97fa6e09a3d52eebe140a234a7620eb933a9c02f
Aman Kumar <108537150+DeveloperBoyAman@users.noreply.github.com>
2023-03-07 12:08:14 +0530

    Update pattern-43.c

--- Programs In C/Iterative Control Statements (Loops)/Star Patterns/pattern-43.c
+++ Programs In C/Iterative Control Statements (Loops)/Star Patterns/pattern-43.c
@@ -1,1 +1,45 @@
+// C Program to Print Following Pattern
+
+/*
+
+    Pattern 43.
+
+
+          *           *
+        *   *       *   *
+      *       *   *       *
+    *           *           *
+  *           *   *           *
+
+*/
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+
+// Main Function Start
+int main()
+{
+
+    int rows, cols;
+    printf("\nHow Many Rows => ");
+    scanf("%d", &rows);
+    cols = rows * rows;
+    puts("\n--------------------------------------------\n");
+    for (int row = 1; row <= rows; row++)
+    {
+        for (int col = 1; col <= cols; col++)
+        {
+            if (col == rows + 1 - row || col == rows - 1 + row || col == rows * 2 + 2 - row || col == rows * 2 + row)
+                printf("* ");
+            else
+                printf("  ");
+        }
+        printf("\n");
+    }
+
+    getch();
+    return 0;
+}
+// Main Function End
 
