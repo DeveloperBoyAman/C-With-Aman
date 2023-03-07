a256923929fe37807d3c00c50c17186893bd5b71
Aman Kumar <108537150+DeveloperBoyAman@users.noreply.github.com>
2023-03-07 11:54:52 +0530

    Update pattern-42.c

--- Programs In C/Iterative Control Statements (Loops)/Star Patterns/pattern-42.c
+++ Programs In C/Iterative Control Statements (Loops)/Star Patterns/pattern-42.c
@@ -1,1 +1,61 @@
+// C Program to Print Following Pattern
+
+/*
+
+    Pattern 42.
+
+       1
+       0 1
+       1 0 1
+       0 1 0 1
+       1 0 1 0 1
+
+
+*/
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+
+// Main Function Start
+int main()
+{
+
+    // //  1st Approach
+    int rows,  num;
+    printf("\nHow Many Rows => ");
+    scanf("%d", &rows);
+    puts("\n--------------------------------------------\n");
+    for (int row = 1; row <= rows; row++)
+    {
+        num = row & 1 ? 1 : 0;
+        for (int col = 1; col <= row; col++)
+        {
+            printf("%d ", num);
+            num = 1 - num;
+        }
+        printf("\n");
+    }
+
+    // //  2nd Approach
+    // int rows, start = 1, num;
+    // printf("\nHow Many Rows => ");
+    // scanf("%d", &rows);
+    // puts("\n--------------------------------------------\n");
+    // for (int row = 1; row <= rows; row++)
+    // {
+    //     num = start;
+    //     for (int col = 1; col <= row; col++)
+    //     {
+    //         printf("%d ", num);
+    //         num = 1 - num;
+    //     }
+    //     start = 1 - start;
+    //     printf("\n");
+    // }
+
+    getch();
+    return 0;
+}
+// Main Function End
 
